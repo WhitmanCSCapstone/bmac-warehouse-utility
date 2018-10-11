be562158e06fc55cabd46db9b5619b7f699a5b90
Ben Limpich <benlimpich@gmail.com>
2018-10-10 23:42:47 -0700

    finish csv to dirty json script

--- compile_json.py
+++ compile_json.py
@@ -1,11 +1,10 @@
 import json
 import glob
 import csv
-
-csv_files = glob.glob("./stub_csvs/*.csv")
+import os
 
 field_names = {
-    "providers": [
+    "Providers": [
         "activity_status",
         "address",
         "city",
@@ -19,32 +18,159 @@ field_names = {
         "type",
         "zip",
     ],
+    "Customers": [
+        "customer_id",
+        "code",
+        "address",
+        "city",
+        "state",
+        "zip",
+        "county",
+        "contact",
+        "phone",
+        "email",
+        "status",
+        "notes"
+    ],
 
+    "FundingSources": [
+        "id",
+        "code"
+    ],
+
+    "Persons": [
+        "unique_id",
+        "username",
+        "last_name",
+        "first_name",
+        "address",
+        "city",
+        "zip",
+        "phone1",
+        "phone2",
+        "email",
+        "type",
+        "status",
+        "notes",
+        "password"],
+
+    "Products": [
+        "product_id",
+        "product_code",
+        "funding_source",
+        "unit_weight",
+        "unit_price",
+        "initial_date",
+        "initial_stock",
+        "minimum_stock",
+        "history",
+        "current_stock",
+        "inventory_date",
+        "status",
+        "notes"
+    ],
+
+    "Providers": [
+        "provider_id",
+        "code",
+        "type",
+        "address",
+        "city",
+        "state",
+        "zip",
+        "county",
+        "contact",
+        "phone",
+        "email",
+        "status",
+        "notes"
+    ],
+
+    "Shipments": [
+        "customer_id",
+        "funds_source",
+        "ship_date",
+        "ship_via",
+        "ship_items",
+        "ship_rate",
+        "total_weight",
+        "total_price",
+        "invoice_date",
+        "invoice_no",
+        "notes"
+    ],
+
+    "Contributions": [
+        "provider_id",
+        "recieve_date",
+        "receive_items",
+        "payment_source",
+        "billed_amt",
+        "notes"
+    ],
 }
 
+file_names = [
+    "Contributions",
+    "Customers",
+    "FundingSources",
+    "Persons", "Products",
+    "Providers",
+    "Shipments",
+]
+
+# erase all the previously written json files
+for name in file_names:
+    open("./stub_json/{}.json".format(name), "w").close()
+
+# get csv files
+csv_files = glob.glob("./stub_csvs/*.csv")
+
+# write new json from csv files
+for f in csv_files:
+
+    csv_file = open(f, "r")
+    clean_name = f[12:-4]
+
+    reader = csv.DictReader(csv_file, field_names[clean_name])
+
+    json_file = open("./stub_json/{}.json".format(clean_name), 'w+')
+
+    json_file.write("{" + '"{}": ['.format(clean_name))
+
+    for row in reader:
+        # json.dump(row, json_file, indent=4, sort_keys=True)
+        json.dump(row, json_file)
+        json_file.write(",")
+
+    # make it valid json (SUPER janky)
+
+    json_file.close()
+
+    json_file = open("./stub_json/{}.json".format(clean_name), 'rb+')
+    json_file.seek(-1, os.SEEK_END)
+    json_file.truncate()
+    json_file.close()
+
+    json_file = open("./stub_json/{}.json".format(clean_name), 'a')
+    json_file.write("]}")
+    json_file.close()
 
 
-#for f in csv_files:
-    #file = open(f, "r")
-    #reader = csv.DictReader( csvfile, fieldnames)
-
-
-
-
-
-
+# get all the new json files
 json_files = glob.glob("./stub_json/*.json")
 
 obj_list = []
 
+# compile json files into one json object
 for f in json_files:
     with open(f, 'rb') as data:
         obj = json.load(data)
         obj_list.append(obj)
 
-# erase the contents of the file
+# erase the contents of the old file
 open("master_json.json", "w").close()
 
-#write the compiled json to master_json.json
+# write the compiled json to master_json.json
 with open("master_json.json", "w") as outfile:
     json.dump(obj_list, outfile, indent=4, sort_keys=True)
